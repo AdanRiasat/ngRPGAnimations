634da6aa13d9e70e47a614abf55feb58b9e3a437
AdanRiasat <6194262@cegepmontpetit.ca>
2025-11-13 11:13:16 -0500

    PROGRES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
+import { shakeX } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger("death", [
+      transition(
+        ":increment",
+        useAnimation(shakeX, { params: { timing: 0.5 } })
+      ),
+    ]),
+  ]
 })
 export class AppComponent {
   slimeIsPresent = false;
+
+  ng_death = 0;
+  ng_attack = 0;
+  ng_preAttack = 0;
 
   constructor() {
   }
@@ -15,13 +28,16 @@ export class AppComponent {
   spawn() {
     this.slimeIsPresent = true;
     // TODO Animation angular avec forwards
+    this.showSlime()
   }
 
   death(){
     this.slimeIsPresent = false;
     // TODO Animation angular avec forwards
+    this.hideSlime()
 
     // TODO 2e animation angular en même temps
+    this.ng_death++
   }
 
   attack(){
@@ -31,5 +47,17 @@ export class AppComponent {
 
   hit(){
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+  }
+
+  showSlime(){
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
+  }
+
+  hideSlime() {
+    let element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeIn");
+    element?.classList.add("fadeOut");
   }
 }
